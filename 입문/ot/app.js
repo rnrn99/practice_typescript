237bdb99ea8fcb892ceb011a342dab1b52e8b4c7
giu0213@naver.com <giu0213@naver.com>
2021-12-13 21:52:03 +0900

    [ADD] typescript orientation

---
+++ 입문/ot/app.js
@@ -0,0 +1,49 @@
+// api url
+var url = "https://jsonplaceholder.typicode.com/users/1";
+
+// dom
+var username = document.querySelector("#username");
+var email = document.querySelector("#email");
+var address = document.querySelector("#address");
+
+// user data
+var user = {};
+
+/**
+ * @typedef {object} Address
+ * @property {string} street
+ * @property {string} city
+ */
+
+/**
+ * @typedef {object} User
+ * @property {string} name
+ * @property {string} email
+ * @property {Address} address
+ */
+
+/**
+ * @returns {Promise<User>}
+ */
+
+function fetchUser() {
+  return axios.get(url);
+}
+
+function startApp() {
+  fetchUser()
+    .then(function (response) {
+      console.log(response);
+      user = response.data;
+      // TODO: 이름, 이메일, 주소 표시하기
+      username.innerText = user.name;
+      email.innerText = user.email;
+      address.innerText =
+        user.address.street + user.address.suite + user.address.city;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+startApp();
